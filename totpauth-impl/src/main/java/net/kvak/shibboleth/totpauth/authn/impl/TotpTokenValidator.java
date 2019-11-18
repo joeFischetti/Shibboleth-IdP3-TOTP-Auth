e887109a93f976105e014fc8b458c81b14934e50
Joe Fischetti <joseph.fischetti@marist.edu>
2019-11-18 08:35:27 -0500

    forking the original project to work with the MFA flow

--- totpauth-impl/src/main/java/net/kvak/shibboleth/totpauth/authn/impl/TotpTokenValidator.java
+++ totpauth-impl/src/main/java/net/kvak/shibboleth/totpauth/authn/impl/TotpTokenValidator.java
@@ -9,16 +9,20 @@ import javax.security.auth.Subject;
 import org.opensaml.profile.context.ProfileRequestContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+import com.google.common.base.Function;
 import com.warrenstrange.googleauth.GoogleAuthenticator;
 
 import net.kvak.shibboleth.totpauth.api.authn.SeedFetcher;
 import net.kvak.shibboleth.totpauth.api.authn.TokenValidator;
 import net.kvak.shibboleth.totpauth.api.authn.context.TokenUserContext;
 import net.kvak.shibboleth.totpauth.api.authn.context.TokenUserContext.AuthState;
+import net.shibboleth.idp.session.context.SessionContext;
+import net.shibboleth.idp.session.context.navigate.CanonicalUsernameLookupStrategy;
+import net.shibboleth.idp.attribute.IdPAttribute;
 import net.shibboleth.idp.authn.AbstractValidationAction;
 import net.shibboleth.idp.authn.AuthnEventIds;
 import net.shibboleth.idp.authn.context.AuthenticationContext;
+import net.shibboleth.idp.authn.context.SubjectContext;
 import net.shibboleth.idp.authn.context.UsernamePasswordContext;
 import net.shibboleth.idp.authn.principal.UsernamePrincipal;
 import net.shibboleth.utilities.java.support.annotation.constraint.NotEmpty;
@@ -57,6 +61,11 @@ public class TotpTokenValidator extends AbstractValidationAction implements Toke
 	@NotEmpty
 	private SeedFetcher seedFetcher;
 
+
+	private String username;
+	private Function<ProfileRequestContext,String> usernameLookupStrategy;
+
+
 	private boolean result = false;
 
 	/** Inject seedfetcher **/
@@ -75,21 +84,58 @@ public class TotpTokenValidator extends AbstractValidationAction implements Toke
 
 	}
 
+    @Override
+    protected boolean doPreExecute(
+            @Nonnull ProfileRequestContext profileRequestContext,
+            @Nonnull AuthenticationContext authenticationContext) {
+
+        if (!super.doPreExecute(profileRequestContext, authenticationContext)) {
+            return false;
+        }
+
+
+	usernameLookupStrategy = new CanonicalUsernameLookupStrategy();
+	username = usernameLookupStrategy.apply(profileRequestContext);
+	
+
+        if (username == null) {
+        	log.info("{} No previous SubjectContext or Principal is set", getLogPrefix());
+        	handleError(profileRequestContext, authenticationContext, "NoCredentials", AuthnEventIds.NO_CREDENTIALS);
+        	return false;
+        }
+        
+    	log.info("{} PrincipalName from SubjectContext is {}", getLogPrefix(), username);
+        return true;
+    }
+
+	@Override
+	protected Subject populateSubject(Subject subject) {
+		log.info("{} TokenValidator populateSubject is called", getLogPrefix());		
+		if (StringSupport.trimOrNull(username) != null) {
+			log.info("{} Populate subject {}", getLogPrefix(), username);
+			subject.getPrincipals().add(new UsernamePrincipal(username));
+			return subject;
+		}
+		return null;
+	}
+
 	@Override
 	protected void doExecute(@Nonnull final ProfileRequestContext profileRequestContext,
 			@Nonnull final AuthenticationContext authenticationContext) {
-		log.debug("{} Entering totpvalidator", getLogPrefix());
+		log.info("{} Entering totpValidator for username: {}", getLogPrefix(),username);
 
 		try {
 
 			TokenUserContext tokenCtx = authenticationContext.getSubcontext(TokenUserContext.class, true);
-			upCtx = authenticationContext.getSubcontext(UsernamePasswordContext.class, true);
+			//upCtx = authenticationContext.getSubcontext(UsernamePasswordContext.class, true);
+			log.info("{} Validating for user:  {}", getLogPrefix(), username);
 
 			/* Add seeds from repository to tokenUserContext */
-			seedFetcher.getSeed(upCtx.getUsername(), tokenCtx);
+			seedFetcher.getSeed(username, tokenCtx);
+			log.info("{} Received the following user token:  {}", getLogPrefix(),tokenCtx.getTokenCode());
 
 			if (tokenCtx.getState() == AuthState.OK) {
-				log.debug("{} Validating user token against seed", getLogPrefix());
+				log.info("{} Validating user: {} provided token:  {} against seed", getLogPrefix(), username, tokenCtx.getTokenSeed());
 				
 				/* Get seeds from tokenUserContext */
 				ArrayList<String> seeds = tokenCtx.getTokenSeed();
@@ -99,23 +145,26 @@ public class TotpTokenValidator extends AbstractValidationAction implements Toke
 				while (it.hasNext()) {
 					result = validateToken(it.next(), tokenCtx.getTokenCode());
 					if (result) {
-						log.info("{} Token authentication success for user: {}", getLogPrefix(), upCtx.getUsername());
+						log.info("{} Token authentication success for user: {}", getLogPrefix(), username);
 						tokenCtx.setState(AuthState.OK);
-						buildAuthenticationResult(profileRequestContext, authenticationContext);
+						//buildAuthenticationResult(profileRequestContext, authenticationContext);
 						return;
 					}
 				}
 			}
+			else{
+				log.info("{} Failed to get tokenCtx state", getLogPrefix());
+			}
 			
 			if (tokenCtx.getState() == AuthState.REGISTER) {
-				log.info("{} User: {} has not registered token", getLogPrefix(), upCtx.getUsername());
+				log.info("{} User: {} has not registered token", getLogPrefix(), username);
 				handleError(profileRequestContext, authenticationContext, "RegisterToken",
 						AuthnEventIds.ACCOUNT_ERROR);
 				return;
 			}
 
 			if (!result) {
-				log.info("{} Token authentication failed for user: {}", getLogPrefix(), upCtx.getUsername());
+				log.info("{} Token authentication failed for user: {}", getLogPrefix(), username);
 				tokenCtx.setState(AuthState.CANT_VALIDATE);
 				handleError(profileRequestContext, authenticationContext, "InvalidCredentials",
 						AuthnEventIds.INVALID_CREDENTIALS);
@@ -123,7 +172,7 @@ public class TotpTokenValidator extends AbstractValidationAction implements Toke
 			}
 
 		} catch (Exception e) {
-			log.warn("{} Login by {} produced exception", getLogPrefix(), upCtx.getUsername(), e);
+			log.warn("{} Login by {} produced exception", getLogPrefix(), username, e);
 			handleError(profileRequestContext, authenticationContext, "InvalidCredentials",
 					AuthnEventIds.INVALID_CREDENTIALS);
 		}
@@ -132,25 +181,14 @@ public class TotpTokenValidator extends AbstractValidationAction implements Toke
 
 	@Override
 	public boolean validateToken(String seed, int token) {
-		log.debug("{} Entering validatetoken", getLogPrefix());
+		log.info("{} Entering validatetoken", getLogPrefix());
 
 		if (seed.length() == 16) {
-			log.debug("{} authorize {} - {} ", getLogPrefix(), seed, token);
+			log.info("{} authorize {} - {} ", getLogPrefix(), seed, token);
 			return gAuth.authorize(seed, token);
 		}
-		log.debug("{} Token code validation failed. Seed is not 16 char long", getLogPrefix());
+		log.info("{} Token code validation failed. Seed is not 16 char long", getLogPrefix());
 		return false;
-	}
-
-	@Override
-	protected Subject populateSubject(Subject subject) {
-		if (StringSupport.trimOrNull(upCtx.getUsername()) != null) {
-			log.debug("{} Populate subject {}", getLogPrefix(), upCtx.getUsername());
-			subject.getPrincipals().add(new UsernamePrincipal(upCtx.getUsername()));
-			return subject;
-		}
-		return null;
-
 	}
 	
 }
--- totpauth-impl/src/main/java/net/kvak/shibboleth/totpauth/authn/impl/seed/DummySeedFetcher.java
+++ totpauth-impl/src/main/java/net/kvak/shibboleth/totpauth/authn/impl/seed/DummySeedFetcher.java
@@ -1,9 +1,13 @@
 package net.kvak.shibboleth.totpauth.authn.impl.seed;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import net.kvak.shibboleth.totpauth.api.authn.SeedFetcher;
 import net.kvak.shibboleth.totpauth.api.authn.context.TokenUserContext;
 
 public class DummySeedFetcher implements SeedFetcher {
+	private final Logger log = LoggerFactory.getLogger(LdapSeedFetcher.class);
+
 
 	public DummySeedFetcher() {
 		// TODO Auto-generated constructor stub
@@ -13,5 +17,7 @@ public class DummySeedFetcher implements SeedFetcher {
 	public void getSeed(String username, TokenUserContext tokenUserCtx) {
 		// Dummy seed for testing
 		tokenUserCtx.setTokenSeed("G24YUKCHHXRDWCPR");
+		log.info("Returning default seed for user {}", username);
+
 	}
 }
