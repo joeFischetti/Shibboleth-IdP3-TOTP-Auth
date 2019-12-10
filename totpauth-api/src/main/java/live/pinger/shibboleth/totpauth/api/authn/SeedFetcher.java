a2601018a37504c7c7c6d91d3872b6d44ef6d466
Joseph Fischetti <joseph.fischetti@marist.edu>
2019-12-10 11:45:35 -0500

    Updating comments

--- totpauth-api/src/main/java/live/pinger/shibboleth/totpauth/api/authn/SeedFetcher.java
+++ totpauth-api/src/main/java/live/pinger/shibboleth/totpauth/api/authn/SeedFetcher.java
@@ -4,6 +4,7 @@ import live.pinger.shibboleth.totpauth.api.authn.context.TokenUserContext;
 
 import org.opensaml.profile.context.ProfileRequestContext;
 
+@SuppressWarnings("rawtypes")
 public interface SeedFetcher {
 	
 	public void getSeed(String username, TokenUserContext tokenUserCtx, String logPrefix,
